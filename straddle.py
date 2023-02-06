99b67a5b4de69c760a53c173eb23fe2d470b2562
Shivam Syal <syals@mcvts.net>
2023-02-05 19:04:09 -0600

    Implementing straddle strategy (long)

---
+++ straddle.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import random
+
+spot = float(input("Enter a spot price: "))
+long_put = round(random.uniform(spot-10, spot), 2)
+long_call = round(random.uniform(spot-10, spot), 2)
+put_prem = round(random.uniform(0.0, 10.0), 2)
+call_prem = round(random.uniform(0.0, 10.0), 2)
+sRange = np.arange(0, spot*2, 1)
+
+def calls(sR, price, prem):
+    return np.where(sR > price, sR - price, 0) - prem
+def puts(sR, price, prem):
+    return np.where(sR < price, price - sR, 0) - prem
+payoff_call = calls(sRange, long_call, call_prem)
+payoff_put = puts(sRange, long_put, put_prem)
+payoff_strad = payoff_call + payoff_put
+fig, ax = plt.subplots()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible('zero')
+ax.plot(sRange, payoff_call, '--', label='Long Call', color='r')
+ax.plot(sRange, payoff_put, '--', label='Long Call', color='g')
+ax.plot(sRange, payoff_strad, label='Straddle', color='b')
+plt.xlabel('Price', ha = 'left')
+plt.ylabel('Profit/Loss')
+plt.legend()
+plt.show()
